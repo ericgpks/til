caef24a58c5df9bb083d00ed8f53b310ccdc0553
ericgpks <sq.wacbc5.nyc@gmail.com>
2023-07-11 17:56:54 +0900

    add rails_test_sample

---
+++ RubyonRails/rails_test_sample/test/models/todo_test.rb
@@ -0,0 +1,27 @@
+require "test_helper"
+
+class TodoTest < ActiveSupport::TestCase
+  test "the truth" do
+    assert true 
+  end
+  
+  test "sleep 1" do
+    sleep 3
+    assert true
+  end
+  
+  test "sleep 2" do
+    sleep 3
+    assert true 
+  end
+  
+  test "sleep3" do
+    sleep 3
+    assert true
+  end
+  
+  test "sleep 4" do
+    sleep 3
+    assert true
+  end
+end
