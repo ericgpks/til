e98798b18135ed0e580b28d0208f19d4348562a6
ericgpks <sq.wacbc5.nyc@gmail.com>
2023-07-11 17:54:35 +0900

    add blog

---
+++ RubyonRails/blog/config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root "articles#index"
+  resources :articles
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
